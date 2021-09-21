ba2347f5989ab02a94b8b506a2ba13c744cc0016
Sam Schnaible II <sam@Sams-MacBook-Pro.local>
2021-09-21 15:32:41 -0700

    completed problem

--- max-consecutive-ones.js
+++ max-consecutive-ones.js
@@ -1,1 +1,11 @@
+/* Input: nums = [1,1,0,1,1,1]
+Output: 3
+Explanation: The first two digits or the last three digits are consecutive 1s.
+The maximum number of consecutive 1s is 3.
+*/
+
+
+const findMaxConsecutiveOnes = nums => {
+  return Math.max(...nums.join("").split('0').map(num => num.length))
+}
 
