0563e5c1bd572bd168d6a57756eff5db699f5256
Sam Schnaible II <sschnaible83@gmail.com>
2021-09-27 18:05:09 -0700

    create str to obj function

---
+++ common-characters.js
@@ -0,0 +1,25 @@
+/**
+ * Write a function `f(a, b)` which takes two or more strings as arguments and returns a
+ * string containing the characters found in both strings (without duplication), in the
+ * order that they appeared in `a`. Remember to skip spaces and characters you
+ * have already encountered!
+ *
+ * Example: commonCharacters('acexivou', 'aegihobu')
+ * Returns: 'aeiou'
+ *
+ */
+
+//Helper function
+//create object from a string with characters as keys with value true
+const stringToObject = string => (
+  string.split('').reduce( (obj, char) => {
+    if ( char.match(/[a-z]/i) ) obj[char] = true;
+    return obj;
+  }, {})
+)
+
+//create object with only common characters from two objects
+
+const commonCharacters = (string1, string2) => {
+
+}
