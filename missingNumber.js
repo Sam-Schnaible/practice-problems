93d88f65ffb8f03bba1fb296165647e33b149ee2
Sam Schnaible II <sschnaible83@gmail.com>
2021-10-01 16:39:02 -0700

    first attempt

--- missingNumber.js
+++ missingNumber.js
@@ -5,7 +5,13 @@ return the only number in the range that is missing from the array.
 
 const missingNumber = nums => {
   //calculate what total should be if number wasn't missing
+  const expectedTotal = nums.length * (nums.length + 1) / 2;
   //declare variable sum
+  let sum;
   //calculate total of nums array with reduce
-  //return difference between two totals
+  const actualTotal = nums.reduce( (sum, num) => {
+    sum += num;
+    return sum;
+  }, 0)
+  return expectedTotal - actualTotal;
 };
