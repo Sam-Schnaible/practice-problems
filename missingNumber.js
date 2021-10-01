4dbb57f0466d1899fbed39dfe10d72216eec2d85
Sam Schnaible II <sschnaible83@gmail.com>
2021-10-01 16:38:45 -0700

    pseudocode

---
+++ missingNumber.js
@@ -0,0 +1,11 @@
+/*
+Given an array nums containing n distinct numbers in the range [0, n],
+return the only number in the range that is missing from the array.
+*/
+
+const missingNumber = nums => {
+  //calculate what total should be if number wasn't missing
+  //declare variable sum
+  //calculate total of nums array with reduce
+  //return difference between two totals
+};
