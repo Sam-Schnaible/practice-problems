ade0cbcb5bbaadd8402fc7b76f727ed64db84fdd
Sam Schnaible II <sschnaible83@gmail.com>
2021-10-02 18:18:51 -0700

    write method to find lesser of children

--- binaryHeap.js
+++ binaryHeap.js
@@ -3,3 +3,13 @@ var BinaryHeap = function () {
   this._compare = function (i, j) { return i < j; };
 };
 
+BinaryHeap.prototype._getLesserChildIndex = function (parentIndex) {
+  var childIndices = [parentIndex * 2 + 1, parentIndex * 2 + 2].filter(function (index) {
+    return index < this._heap.length;
+  }, this);
+  if (this._compare(this._heap[childIndices[0]], this._heap[childIndices[1]])) {
+    return childIndices[0];
+  } else {
+    return childIndices[1];
+  }
+};
