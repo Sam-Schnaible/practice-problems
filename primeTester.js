12d47211a5e68be8e46e6ba52111646a4f8b12a6
Sam Schnaible II <sschnaible83@gmail.com>
2021-10-05 20:46:14 -0700

    problem outline

---
+++ primeTester.js
@@ -0,0 +1,20 @@
+/**
+ * A prime number is a whole number that has no other divisors other than
+ * itself and 1. Write a function that accepts a number and returns true if it's
+ * a prime number, false if it's not.
+ */
+
+ var primeTester = function(n) {
+
+  };
+
+
+  /* Write a function that generates a list of all prime numbers
+ * in a user-specified range (inclusive).
+ */
+
+var primeSieve = function (start, end) {
+
+  };
+
+
