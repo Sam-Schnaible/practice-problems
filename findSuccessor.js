551cc3a64f5326cd08bbf9ef029ab5afe71bb172
Sam Schnaible II <sschnaible83@gmail.com>
2021-12-31 17:30:13 -0800

    write class for binary tree

---
+++ findSuccessor.js
@@ -0,0 +1,15 @@
+//AlgoExpert
+
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+    this.parent = null;
+  }
+}
+
+const findSuccessor = (tree, node) => {
+
+}
+
